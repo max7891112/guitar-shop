68792413443dc1975570121b95d70096746365ce
max7891112 <max.alkhimovich@gmail.com>
2022-11-02 11:51:34 +0300

    base structure

---
+++ constants/catalog.js
@@ -0,0 +1,20 @@
+const CATALOG = [
+    {
+        id: 'el1',
+        name: 'FENDER SQUIER MM Stratocaster Black',
+        img: '',
+        price: 17400,
+    },
+    {
+        id: 'el2',
+        name: '',
+        img: '',
+        price: 0,
+    },
+    {
+        id: 'el3',
+        name: '',
+        img: '',
+        price: 0,
+    },
+]
